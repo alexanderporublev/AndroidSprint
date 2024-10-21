0dc4b47a933c26aab7a84e6490a2ad92e0864219
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-10-21 10:23:12 +0300

    RA-17 shared preferences

--- app/src/main/java/ru/redsoft/androidsprint/RecipeFragment.kt
+++ app/src/main/java/ru/redsoft/androidsprint/RecipeFragment.kt
@@ -1,6 +1,7 @@
 package ru.redsoft.androidsprint
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
@@ -14,10 +15,10 @@ import com.google.android.material.divider.MaterialDividerItemDecoration
 import ru.redsoft.androidsprint.databinding.FragmentRecipeBinding
 import ru.redsoft.androidsprint.models.Recipe
 
-class RecipeFragment: Fragment() {
+class RecipeFragment : Fragment() {
 
     private var recipe: Recipe? = null
-    private var isFavorite = false
+
 
     val binding: FragmentRecipeBinding by lazy { FragmentRecipeBinding.inflate(layoutInflater) }
 
@@ -82,23 +83,48 @@ class RecipeFragment: Fragment() {
                 ingredientsAdapter.updateIngredients(progress)
             }
 
-            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            override fun onStartTrackingTouch(seekBar: SeekBar) = Unit
 
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar) {
-
-            }
+            override fun onStopTrackingTouch(seekBar: SeekBar) = Unit
         })
 
         switchFavoriteIcon()
         binding.addToFavoriteButton.setOnClickListener {
-            isFavorite = !isFavorite
+            getFavorites()?.let { favorites ->
+                if (favorites.contains(recipe?.id.toString()))
+                    saveFavorites(favorites - recipe?.id.toString())
+                else
+                    saveFavorites(favorites + recipe?.id.toString())
+            }
             switchFavoriteIcon()
         }
 
     }
 
-    private fun switchFavoriteIcon() = binding.addToFavoriteButton.setImageDrawable(context?.resources?.getDrawable(if (isFavorite) R.drawable.ic_heart else R.drawable.ic_heart_empty, context?.theme))
+    private fun saveFavorites(ids: Set<String>) = activity?.let { activity ->
+        val sharedPrefs = activity.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val editor = sharedPrefs.edit()
+        editor.putStringSet(PREFS_FAVORITE_IDS, ids)
+        editor.apply()
+    }
 
+    private fun getFavorites() = activity?.let { activity ->
+        val sharedPrefs = activity.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        sharedPrefs.getStringSet(PREFS_FAVORITE_IDS, emptySet())?.let { HashSet<String>(it) }
+            ?: HashSet()
+    }
+
+    private fun switchFavoriteIcon() = binding.addToFavoriteButton.setImageDrawable(
+        context?.resources?.getDrawable(
+            if (isFavorite()) R.drawable.ic_heart else R.drawable.ic_heart_empty,
+            context?.theme
+        )
+    )
+
+    private fun isFavorite() = getFavorites()?.contains(recipe?.id.toString()) ?: false
+
+    companion object {
+        const val PREFS_NAME = "recipes_preferences"
+        const val PREFS_FAVORITE_IDS = "favorite_ids"
+    }
 }
